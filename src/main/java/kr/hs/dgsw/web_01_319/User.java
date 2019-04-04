7c1dd84411d1ce4298b05411b133ee5dfd7dd0c8
JEJ <eunjin10714@daum.net>
2019-04-04 11:57:56 +0900

    User

--- src/main/java/kr/hs/dgsw/web_01_319/User.java
+++ src/main/java/kr/hs/dgsw/web_01_319/User.java
@@ -5,6 +5,7 @@ public class User {
     private String id;
     private String name;
     private String email;
+    private String password;
 
     public User(){}
 
@@ -32,4 +33,12 @@ public class User {
     public String getEmail() {return email;}
 
     public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
